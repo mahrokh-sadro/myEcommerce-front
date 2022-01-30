9a3f2a7ee36d29f8c786101956d764b8c2f607bc
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-01-30 00:07:57 -0500

    working on signin

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,39 +1,41 @@
-// import * as React from "react";
-// import { useState } from "react";
-// import Avatar from "@mui/material/Avatar";
-// import Button from "@mui/material/Button";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import TextField from "@mui/material/TextField";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import Checkbox from "@mui/material/Checkbox";
-// import Link from "@mui/material/Link";
-// import Grid from "@mui/material/Grid";
-// import Box from "@mui/material/Box";
-// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-// import Typography from "@mui/material/Typography";
-// import Container from "@mui/material/Container";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import { authenticate } from "./apiCore";
+import * as React from "react";
+import { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-// function Copyright(props) {
-//   return (
-//     <Typography
-//       variant="body2"
-//       color="text.secondary"
-//       align="center"
-//       {...props}
-//     >
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
+import { Redirect } from "react-router-dom";
 
-// const theme = createTheme();
+import { signin, authenticate, isAuthenticated } from "./apiCore";
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
 
 // const LoginForm = () => {
 //   const [formData, setFormData] = useState({
@@ -149,11 +151,6 @@
 
 // export default LoginForm;
 
-import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
-
-import { signin, authenticate, isAuthenticated } from "./apiCore";
-
 const LoginForm = () => {
   const [values, setValues] = useState({
     email: "",
@@ -184,37 +181,41 @@ const LoginForm = () => {
           });
           // alert(values);
           console.log(values);
+          setValues({
+            email: "",
+            password: "",
+          });
         });
       }
     });
   };
 
-  const signUpForm = () => (
-    <form>
-      <div className="form-group">
-        <label className="text-muted">Email</label>
-        <input
-          onChange={handleChange("email")}
-          type="email"
-          className="form-control"
-          value={email}
-        />
-      </div>
+  // const signUpForm = () => (
+  //   <form>
+  //     <div className="form-group">
+  //       <label className="text-muted">Email</label>
+  //       <input
+  //         onChange={clickSubmit}
+  //         type="email"
+  //         className="form-control"
+  //         value={email}
+  //       />
+  //     </div>
 
-      <div className="form-group">
-        <label className="text-muted">Password</label>
-        <input
-          onChange={handleChange("password")}
-          type="password"
-          className="form-control"
-          value={password}
-        />
-      </div>
-      <button onClick={clickSubmit} className="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  );
+  //     <div className="form-group">
+  //       <label className="text-muted">Password</label>
+  //       <input
+  //         onChange={handleChange("password")}
+  //         type="password"
+  //         className="form-control"
+  //         value={password}
+  //       />
+  //     </div>
+  //     <button onClick={clickSubmit} className="btn btn-primary">
+  //       Submit
+  //     </button>
+  //   </form>
+  // );
 
   const showError = () => (
     <div
@@ -246,24 +247,108 @@ const LoginForm = () => {
   // };
 
   const redirectUser = () => {
-    if (redirectToReferrer) {
-      if (user && user.role === 1) {
-        return <Redirect to="https://www.google.com/" />;
-      } else {
-        return <Redirect to="https://www.google.com/" />;
-      }
-    }
-    if (isAuthenticated()) {
-      return <Redirect to="https://www.google.com/" />;
-    }
+    return <Redirect to="/products" />;
   };
 
   return (
     <>
       {showLoading()}
       {showError()}
-      {signUpForm()}
-      {redirectUser()}
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleChange("email")}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(event) =>
+                  setValues({
+                    ...values,
+                    error: false,
+                    email: event.target.value,
+                  })
+                }
+              />
+
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(event) =>
+                  setValues({
+                    ...values,
+                    error: false,
+                    password: event.target.value,
+                  })
+                }
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                onClick={clickSubmit}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              {/* <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+              </button> */}
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
+      {/* {signUpForm()} */}
+      {/* {redirectUser()} */}
     </>
   );
 };
