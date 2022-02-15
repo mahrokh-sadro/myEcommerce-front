810b9b7825288274d1976575b93adebcbdd5f8cd
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-15 17:40:21 -0500

    deploy

--- src/components/RelatedProducts.js
+++ src/components/RelatedProducts.js
@@ -18,30 +18,28 @@ const RelatedProducts = ({ id }) => {
   return (
     <>
       {relatedProducts?.map((e) => (
-        <Link to={`/product/details/${e._id}`}>
-          <div className="col mb-5">
-            <div className="card h-100">
-              {/* <!-- Product image--> */}
-              <img
-                className="card-img-top"
-                src={e.photoURL}
-                alt="..."
-                height="200rem"
-                objectFit="cover"
-              />
-              {/* <!-- Product details--> */}
-              <div className="card-body p-4">
-                <div className="text-center">
-                  {/* <!-- Product name--> */}
-                  <h5 className="fw-bolder">{e.name}</h5>
-                  {/* <!-- Product price--> */}${e.price}
-                </div>
+        <div className="col mb-5">
+          <div className="card h-100">
+            {/* <!-- Product image--> */}
+            <img
+              className="card-img-top"
+              src={e.photoURL}
+              alt="..."
+              height="200rem"
+              objectFit="cover"
+            />
+            {/* <!-- Product details--> */}
+            <div className="card-body p-4">
+              <div className="text-center">
+                {/* <!-- Product name--> */}
+                <h5 className="fw-bolder">{e.name}</h5>
+                {/* <!-- Product price--> */}${e.price}
               </div>
-              {/* <!-- Product actions--> */}
-              <div className="card-footer p-4 pt-0 border-top-0 bg-transparent"></div>
             </div>
+            {/* <!-- Product actions--> */}
+            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent"></div>
           </div>
-        </Link>
+        </div>
       ))}
     </>
   );
