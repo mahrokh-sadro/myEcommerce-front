7640099ef8930294e4581fe073337ca869b5aa53
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-02-15 00:40:09 -0500

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import RegistrationPage from "../pages/RegistrationPage";
