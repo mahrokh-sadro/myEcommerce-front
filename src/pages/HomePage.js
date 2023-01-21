647aba5c83c13d55b9ca8f2e2fcbea88cdbff95e
mahrokh sadro <msadrolodabaee@myseneca.ca>
2023-01-21 14:08:13 -0500

    final changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,7 +15,7 @@ const HomePage = (props) => {
   const [featuredProducts, setFeaturedProducts] = useState([{}]);
 
   useEffect(() => {
-    fetch(`https://emart-api.herokuapp.com/products?isBestSeller=yes`)
+    fetch(`https://eemart.herokuapp.com/products?isBestSeller=yes`)
       .then((response) => response.json())
       .then((json) => {
         props.setProducts(json.data);
@@ -27,7 +27,7 @@ const HomePage = (props) => {
   }, []);
 
   useEffect(() => {
-    fetch(`https://emart-api.herokuapp.com/products/categories`)
+    fetch(`https://eemart.herokuapp.com/products/categories`)
       .then((response) => response.json())
       .then((json) => {
         console.log(`useEffect`);
@@ -39,7 +39,7 @@ const HomePage = (props) => {
   }, []);
 
   useEffect(() => {
-    fetch(`https://emart-api.herokuapp.com/products?isFeatured=yes`)
+    fetch(`https://eemart.herokuapp.com/products?isFeatured=yes`)
       .then((res) => res.json())
       .then((json) => {
         setFeaturedProducts(json.data);
